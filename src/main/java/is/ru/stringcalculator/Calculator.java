6254d859f5b2e34f532b2607077cce72e558efbd
birna14 <birna14@ru.is>
2017-10-09 13:34:21 +0000

    One tested and two numbers added together tested

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,6 +3,25 @@ package is.ru.stringcalculator;
 public class Calculator {
 	
 	public static int add(String text) {
-		return 0;
+		if(text.equals("")) {
+			return 0;
+		}
+		else {
+			if(text.contains(",")) {
+				String numbers[] = text.split(",");
+				int total = 0;
+				for(String number : numbers){
+					total += toInt(number);
+				}
+				return total; 
+			}
+			else {
+				return toInt(text);
+			}	
+		}
+	}
+	
+	private static int toInt(String number) {
+		return Integer.parseInt(number);
 	}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -9,4 +9,21 @@ public class CalculatorTest {
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
+	
+	@Test
+	public void testOneNumber(){
+		int number = 1;
+		assertEquals(number, Calculator.add("1"));
+		number = 2;
+		assertEquals(number, Calculator.add("2"));
+		
+	}
+	
+	@Test
+	public void testTwoNumber(){
+		int sum = 3;
+		assertEquals(sum, Calculator.add("1,2"));
+		sum = 4;
+		assertEquals(sum, Calculator.add("2,2"));
+	}
 }
